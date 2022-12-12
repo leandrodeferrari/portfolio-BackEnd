bd25b6292e3fd8a86d9ac57a40c7c69378566112
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-12 15:55:35 -0300

    YP-03: Creation repository IUserRepository

---
+++ backend/src/main/java/com/portfolio/security/repository/IUserRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.security.repository;
+
+import com.portfolio.security.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Integer> {
+
+
+
+}
