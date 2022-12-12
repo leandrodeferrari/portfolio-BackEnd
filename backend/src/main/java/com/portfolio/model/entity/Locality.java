3d87deb1463b2e8186b84f379c969b80aaf30a43
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-12 17:11:49 -0300

    YP-04: Creation entities Address, City, Country, Locality and Province

---
+++ backend/src/main/java/com/portfolio/model/entity/Locality.java
@@ -0,0 +1,21 @@
+package com.portfolio.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Table(name = "localities")
+@Entity
+public class Locality implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+
+    @Column(length = 30, name = "LOCALITY_NAME", nullable = false, unique = true)
+    private String name;
+
+}
