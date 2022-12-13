feccea0146a860c786888fec58ff1229a76408fd
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-13 19:23:29 -0300

    YP-11: Creation enum SeniorityTypeEnum

---
+++ backend/src/main/java/com/portfolio/model/enums/SeniorityTypeEnum.java
@@ -0,0 +1,14 @@
+package com.portfolio.model.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SeniorityTypeEnum {
+
+    TRAINEE("Trainee"), JUNIOR("Junior"), SEMISENIOR("SemiSenior"), SENIOR("Senior");
+
+    private final String name;
+
+}
