234d125321a8460939d169a58fe317dbb56f44be
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-13 19:32:42 -0300

    YP-11: Create model and repository of JobExperience (#14)
    * YP-11: Creation entity SeniorityType
    
    * YP-11: Creation enum SeniorityTypeEnum
    
    * YP-11: Creation repository ISeniorityTypeRepository
    
    * YP-11: Creation entity JobExperienceType
    
    * YP-11: Creation enum JobExperienceTypeEnum
    
    * YP-11: Creation repository IJobExperienceTypeRepository
    
    * YP-11: Creation entity JobExperience
    
    * YP-11: Creation repository IJobExperienceRepository

---
+++ backend/src/main/java/com/portfolio/model/entity/JobExperienceType.java
@@ -0,0 +1,21 @@
+package com.portfolio.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Table(name = "job_experience_types")
+@Entity
+public class JobExperienceType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+
+    @Column(length = 30, name = "JOB_EXPERIENCE_TYPE_NAME", nullable = false, unique = true)
+    private String name;
+
+}
