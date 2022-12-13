ad0885b25f8e399ef8fd3ce607be83f82028e795
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-13 01:19:27 -0300

    YP-10: Creation repository IProjectRepository

---
+++ backend/src/main/java/com/portfolio/repository/IProjectRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.repository;
+
+import com.portfolio.model.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProjectRepository extends JpaRepository<Project, Integer> {
+
+
+
+}
