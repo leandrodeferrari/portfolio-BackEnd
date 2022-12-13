d56b5a036717d92f71421ecdf325d9e111a691dc
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-12 23:34:14 -0300

    YP-08: Create model and repository of Course (#10)
    * YP-08: Creation entity InstituteUniversity
    
    * YP-08: Creation repository IInstituteUniversityRepository
    
    * YP-08: Creation entity Course
    
    * YP-08: Creation repository ICourseRepository

---
+++ backend/src/main/java/com/portfolio/repository/ICourseRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.repository;
+
+import com.portfolio.model.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICourseRepository extends JpaRepository<Course, Integer> {
+
+
+
+}
