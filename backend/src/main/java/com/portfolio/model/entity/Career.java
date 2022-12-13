4a35678249a110b9675c5afe539efb827043d642
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-13 00:12:27 -0300

    YP-09: Creation entity Career

---
+++ backend/src/main/java/com/portfolio/model/entity/Career.java
@@ -0,0 +1,57 @@
+package com.portfolio.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Table(name = "careers", uniqueConstraints =
+@UniqueConstraint(columnNames = {"CAREER_NAME", "INSTITUTE_UNIVERSITY_ID"})
+)
+@Entity
+public class Career implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+
+    @Column(length = 50, name = "CAREER_NAME", nullable = false)
+    private String name;
+
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE")
+    private LocalDateTime endDate;
+
+    @Column(name = "IS_FINISHED", nullable = false)
+    private boolean isFinished;
+
+    @Column(name = "APPROVED_SUBJECTS", nullable = false)
+    private Byte approvedSubjects;
+
+    @Column(name = "TOTAL_SUBJECTS", nullable = false)
+    private Byte totalSubjects;
+
+    @Column(name = "STUDY_PLAN_URL", nullable = false)
+    private String studyPlanUrl;
+
+    @Column(name = "DESCRIPT", nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "PERSON_ID", nullable = false)
+    private Person person;
+
+    @ManyToOne
+    @JoinColumn(name = "INSTITUTE_UNIVERSITY_ID", nullable = false)
+    private InstituteUniversity instituteUniversity;
+
+    @ManyToOne
+    @JoinColumn(name = "CAREER_TYPE_ID", nullable = false)
+    private CareerType careerType;
+
+}
