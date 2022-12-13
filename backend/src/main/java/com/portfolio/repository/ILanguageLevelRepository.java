11eb317da9c863e0b78a2f0628874df49ec0d022
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-12 22:34:30 -0300

    YP-07: Creation repository ILanguageLevelRepository

---
+++ backend/src/main/java/com/portfolio/repository/ILanguageLevelRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.repository;
+
+import com.portfolio.model.entity.LanguageLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ILanguageLevelRepository extends JpaRepository<LanguageLevel, Integer> {
+
+
+
+}
