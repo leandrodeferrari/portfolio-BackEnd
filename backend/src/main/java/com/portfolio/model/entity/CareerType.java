7eab5675f8986f225fa5fb1c5497533a2de0f5f5
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-13 00:23:45 -0300

    YP-09: Create model and repository of Career (#11)
    * YP-09: Creation entity CareerType
    
    * YP-09: Creation enum CareerTypeEnum
    
    * YP-09: Creation repository ICareerTypeRepository
    
    * YP-09: Creation entity Career
    
    * YP-09: Creation repository ICareerRepository

---
+++ backend/src/main/java/com/portfolio/model/entity/CareerType.java
@@ -0,0 +1,21 @@
+package com.portfolio.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Table(name = "career_types")
+@Entity
+public class CareerType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+
+    @Column(length = 30, name = "CAREER_TYPE_NAME", nullable = false, unique = true)
+    private String name;
+
+}
---
+++ backend/src/main/java/com/portfolio/model/enums/CareerTypeEnum.java
@@ -0,0 +1,14 @@
+package com.portfolio.model.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CareerTypeEnum {
+
+    ASSOCIATE_DEGREE("Tecnicatura"), ENGINEERING("Ingeniería"), BACHELORS_DEGREE("Licenciatura");
+
+    private final String name;
+
+}
