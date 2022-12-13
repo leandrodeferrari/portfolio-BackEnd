b98f07d250a5d5b6ebfae38b1f6c0ae264ccaedf
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-12 22:04:08 -0300

    YP-06: Create model and repository of Technology (#7)
    * YP-06: Creation entity Technology
    
    * YP-06: Creation repository ITechnologyRepository

---
+++ backend/src/main/java/com/portfolio/model/entity/Technology.java
@@ -0,0 +1,28 @@
+package com.portfolio.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Table(name = "technologies")
+@Entity
+public class Technology implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+
+    @Column(length = 30, name = "TECHNOLOGY_NAME", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "LOGO_URL", nullable = false)
+    private String logoUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "PERSON_ID", nullable = false)
+    private Person person;
+
+}
