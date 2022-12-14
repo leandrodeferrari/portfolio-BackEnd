0ea642c292961db1f8254f0fa0c10681abedcd05
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-14 01:32:30 -0300

    YP-14: Create endpoints login and logout (#17)
    * YP-14: Creation DTOs for Login
    
    * YP-14: Method login and logout added in IAuthService and AuthServiceImpl
    
    * YP-14: Endpoints login and logout added in AuthController

---
+++ backend/src/main/java/com/portfolio/security/service/IAuthService.java
@@ -0,0 +1,15 @@
+package com.portfolio.security.service;
+
+import com.portfolio.security.dto.LoginInDto;
+import com.portfolio.security.dto.LoginOutDto;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface IAuthService {
+
+    LoginOutDto login(LoginInDto loginInDto);
+
+    void logout(HttpServletRequest request, HttpServletResponse response);
+
+}
---
+++ backend/src/main/java/com/portfolio/security/service/impl/AuthServiceImpl.java
@@ -0,0 +1,62 @@
+package com.portfolio.security.service.impl;
+
+import com.portfolio.security.dto.LoginInDto;
+import com.portfolio.security.dto.LoginOutDto;
+import com.portfolio.security.jwt.JwtTokenProvider;
+import com.portfolio.security.model.UserDetailsImpl;
+import com.portfolio.security.service.IAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Service
+public class AuthServiceImpl implements IAuthService {
+
+    private final AuthenticationManager authManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    public AuthServiceImpl(AuthenticationManager authManager,
+                           JwtTokenProvider jwtTokenProvider,
+                           UserDetailsServiceImpl userDetailsServiceImpl){
+        this.authManager = authManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
+    }
+
+    @Override
+    public LoginOutDto login(LoginInDto loginInDto) {
+
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginInDto.getUserNameOrEmail(),
+                        loginInDto.getPassword()));
+
+        UserDetailsImpl userDetailsImpl = userDetailsServiceImpl.
+                loadUserByUsername(loginInDto.getUserNameOrEmail());
+
+        String token = jwtTokenProvider.generateToken(userDetailsImpl);
+
+        return new LoginOutDto(userDetailsImpl.getUsername(), token);
+
+    }
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+    }
+
+}
