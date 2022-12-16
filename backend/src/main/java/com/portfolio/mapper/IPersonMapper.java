0c301627d727d16fb71f9c1db7fb839d1b01bcbc
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-16 12:09:58 -0300

    YP-16: Minor changes in IPersonMapper, PersonServiceImpl and creation PersonUtil

--- backend/src/main/java/com/portfolio/mapper/IPersonMapper.java
+++ backend/src/main/java/com/portfolio/mapper/IPersonMapper.java
@@ -2,13 +2,16 @@ package com.portfolio.mapper;
 
 import com.portfolio.dto.PersonDto;
 import com.portfolio.model.entity.Person;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        uses = {IAddressMapper.class})
 public interface IPersonMapper {
 
-    @Mapping(target = "addressDto", ignore = true)
+    @Mapping(target = "addressDto", source = "address")
     PersonDto personToPersonDto(Person person);
 
 }
