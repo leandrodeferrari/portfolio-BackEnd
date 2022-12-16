1014e02b0862ce58662c3f41ca9ba26eb9ae0411
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-16 00:38:23 -0300

    YP-15: Creation endpoint getPerson in PersonController

---
+++ backend/src/main/java/com/portfolio/controller/PersonController.java
@@ -0,0 +1,27 @@
+package com.portfolio.controller;
+
+import com.portfolio.dto.PersonDto;
+import com.portfolio.service.IPersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+
+    private final IPersonService personService;
+
+    @Autowired
+    public PersonController(IPersonService personService){
+        this.personService = personService;
+    }
+
+    @GetMapping
+    public ResponseEntity<PersonDto> getPerson(){
+        return ResponseEntity.ok().body(personService.getPerson());
+    }
+
+}
