8d63f602d02a55e8adfad3de4a76e2c8f1760839
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-16 13:23:01 -0300

    YP-17: Creation mapper IInstituteUniversityMapper

---
+++ backend/src/main/java/com/portfolio/mapper/IInstituteUniversityMapper.java
@@ -0,0 +1,17 @@
+package com.portfolio.mapper;
+
+import com.portfolio.dto.InstituteUniversityDto;
+import com.portfolio.model.entity.InstituteUniversity;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        uses = {IAddressMapper.class})
+public interface IInstituteUniversityMapper {
+
+    @Mapping(target = "addressDto", source = "address")
+    InstituteUniversityDto instituteUniversityToInstituteUniversityDto(InstituteUniversity instituteUniversity);
+
+}
