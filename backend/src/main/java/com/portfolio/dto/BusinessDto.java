d3feea47abe5f3b56900ca5606f5ab6ea0b95509
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-16 12:11:51 -0300

    YP-16: Creation DTO BusinessDto

---
+++ backend/src/main/java/com/portfolio/dto/BusinessDto.java
@@ -0,0 +1,12 @@
+package com.portfolio.dto;
+
+import lombok.Data;
+
+@Data
+public class BusinessDto {
+
+    private String name;
+    private String logoUrl;
+    private AddressDto addressDto;
+
+}
