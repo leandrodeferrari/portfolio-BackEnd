2f8e66416323dd411cf92e13cc86e31d0d5fc228
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-18 11:57:09 -0300

    Fix/YP-15: Minor changes in PersonController and PersonUtil (#23)
    * YP-15: Annotation UtilityClass deleted in PersonUtil
    
    * YP-15: Endpoint getAll modified in PersonController

--- backend/src/main/java/com/portfolio/util/PersonUtil.java
+++ backend/src/main/java/com/portfolio/util/PersonUtil.java
@@ -1,10 +1,12 @@
 package com.portfolio.util;
 
-import lombok.experimental.UtilityClass;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-@UtilityClass
-public class PersonUtil {
+public final class PersonUtil {
+
+    private PersonUtil(){
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
 
     public static String getEmail(){
         return SecurityContextHolder.getContext().getAuthentication().getName();
