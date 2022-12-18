08b37750a33b200d921cc090a4baeae255b60099
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-18 11:40:00 -0300

    YP-18: Creation mapper ICourseMapper

---
+++ backend/src/main/java/com/portfolio/mapper/ICourseMapper.java
@@ -0,0 +1,22 @@
+package com.portfolio.mapper;
+
+import com.portfolio.dto.CourseDto;
+import com.portfolio.model.entity.Course;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        uses = {IInstituteUniversityMapper.class})
+public interface ICourseMapper {
+
+    @Mappings({
+            @Mapping(target = "startDate", dateFormat = "dd.MM.yyyy"),
+            @Mapping(target = "endDate", dateFormat = "dd.MM.yyyy"),
+            @Mapping(target = "instituteUniversityDto", source = "instituteUniversity")
+    })
+    CourseDto courseToCourseDto(Course course);
+
+}
