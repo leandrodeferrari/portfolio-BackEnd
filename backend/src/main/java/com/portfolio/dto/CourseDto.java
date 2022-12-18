42cbc789dec22da7afde754b0c0d1e3340754b55
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-18 11:39:35 -0300

    YP-18: Creation DTO CourseDto

---
+++ backend/src/main/java/com/portfolio/dto/CourseDto.java
@@ -0,0 +1,17 @@
+package com.portfolio.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class CourseDto implements Serializable {
+
+    private String title;
+    private boolean isApproved;
+    private String certificateUrl;
+    private String startDate;
+    private String endDate;
+    private InstituteUniversityDto instituteUniversityDto;
+
+}
