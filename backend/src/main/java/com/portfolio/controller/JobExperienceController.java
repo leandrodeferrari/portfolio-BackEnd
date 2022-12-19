e21ceaf1426ccda007f0909c8bc129bd4e623574
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-19 09:35:55 -0300

    YP-23: Endpoints getAllTypes and getAllSeniorityTypes added in JobExperienceController

--- backend/src/main/java/com/portfolio/controller/JobExperienceController.java
+++ backend/src/main/java/com/portfolio/controller/JobExperienceController.java
@@ -2,6 +2,8 @@ package com.portfolio.controller;
 
 import com.portfolio.dto.JobExperienceDto;
 import com.portfolio.service.IJobExperienceService;
+import com.portfolio.service.IJobExperienceTypeService;
+import com.portfolio.service.ISeniorityTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,15 +17,31 @@ import java.util.List;
 public class JobExperienceController {
 
     private final IJobExperienceService jobExperienceService;
+    private final IJobExperienceTypeService jobExperienceTypeService;
+    private final ISeniorityTypeService seniorityTypeService;
 
     @Autowired
-    public JobExperienceController(IJobExperienceService jobExperienceService){
+    public JobExperienceController(IJobExperienceService jobExperienceService,
+                                   IJobExperienceTypeService jobExperienceTypeService,
+                                   ISeniorityTypeService seniorityTypeService){
         this.jobExperienceService = jobExperienceService;
+        this.jobExperienceTypeService = jobExperienceTypeService;
+        this.seniorityTypeService = seniorityTypeService;
     }
 
     @GetMapping
     public ResponseEntity<List<JobExperienceDto>> getAll(){
         return ResponseEntity.ok().body(jobExperienceService.getAll());
+    }
+
+    @GetMapping("/types")
+    public ResponseEntity<List<String>> getAllTypes(){
+        return ResponseEntity.ok().body(jobExperienceTypeService.getAllTypes());
+    }
+
+    @GetMapping("/senioritys/types")
+    public ResponseEntity<List<String>> getAllSeniorityTypes(){
+        return ResponseEntity.ok().body(seniorityTypeService.getAllSeniorityTypes());
     }
 
 }
