85b6d3191fa9159583e1e060335b12661c6d2071
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-19 09:54:12 -0300

    YP-24: Endpoint getAllTypes added in ProjectController

--- backend/src/main/java/com/portfolio/controller/ProjectController.java
+++ backend/src/main/java/com/portfolio/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.portfolio.controller;
 
 import com.portfolio.dto.ProjectDto;
 import com.portfolio.service.IProjectService;
+import com.portfolio.service.IProjectTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,15 +16,23 @@ import java.util.List;
 public class ProjectController {
 
     private final IProjectService projectService;
+    private final IProjectTypeService projectTypeService;
 
     @Autowired
-    public ProjectController(IProjectService projectService){
+    public ProjectController(IProjectService projectService,
+                             IProjectTypeService projectTypeService){
         this.projectService = projectService;
+        this.projectTypeService = projectTypeService;
     }
 
     @GetMapping
     public ResponseEntity<List<ProjectDto>> getAll(){
         return ResponseEntity.ok().body(projectService.getAll());
+    }
+
+    @GetMapping("/types")
+    public ResponseEntity<List<String>> getAllTypes(){
+        return ResponseEntity.ok().body(projectTypeService.getAllTypes());
     }
 
 }
