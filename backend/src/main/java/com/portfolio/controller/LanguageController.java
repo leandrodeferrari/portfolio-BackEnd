f04e5dad3f159448b24f7b235faba99245db5e83
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-19 10:08:13 -0300

    YP-25: Endpoint getAllLevels added in LanguageController

--- backend/src/main/java/com/portfolio/controller/LanguageController.java
+++ backend/src/main/java/com/portfolio/controller/LanguageController.java
@@ -1,6 +1,7 @@
 package com.portfolio.controller;
 
 import com.portfolio.dto.LanguageDto;
+import com.portfolio.service.ILanguageLevelService;
 import com.portfolio.service.ILanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +15,24 @@ import java.util.List;
 @RequestMapping("/languages")
 public class LanguageController {
 
+    private final ILanguageLevelService languageLevelService;
     private final ILanguageService languageService;
 
     @Autowired
-    public LanguageController(ILanguageService languageService){
+    public LanguageController(ILanguageLevelService languageLevelService,
+                              ILanguageService languageService){
+        this.languageLevelService = languageLevelService;
         this.languageService = languageService;
     }
 
     @GetMapping
     public ResponseEntity<List<LanguageDto>> getAll(){
         return ResponseEntity.ok().body(languageService.getAll());
+    }
+
+    @GetMapping("/levels")
+    public ResponseEntity<List<String>> getAllLevels(){
+        return ResponseEntity.ok().body(languageLevelService.getAllLevels());
     }
 
 }
