a199501f34667789ff710ba78e3ec854c643df88
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-19 09:39:37 -0300

    YP-23: Create endpoints for get all types and get all seniority types of Job Experience (#31)
    * YP-23: Method getAllTypes added in IJobExperienceTypeService and JobExperienceTypeServiceImpl
    
    * YP-23: Method getAllSeniorityTypes added in ISeniorityTypeService and SeniorityTypeServiceImpl
    
    * YP-23: Endpoints getAllTypes and getAllSeniorityTypes added in JobExperienceController

---
+++ backend/src/main/java/com/portfolio/service/impl/JobExperienceTypeServiceImpl.java
@@ -0,0 +1,29 @@
+package com.portfolio.service.impl;
+
+import com.portfolio.model.entity.JobExperienceType;
+import com.portfolio.repository.IJobExperienceTypeRepository;
+import com.portfolio.service.IJobExperienceTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobExperienceTypeServiceImpl implements IJobExperienceTypeService {
+
+    private final IJobExperienceTypeRepository jobExperienceTypeRepository;
+
+    @Autowired
+    public JobExperienceTypeServiceImpl(IJobExperienceTypeRepository jobExperienceTypeRepository){
+        this.jobExperienceTypeRepository = jobExperienceTypeRepository;
+    }
+
+    @Override
+    public List<String> getAllTypes() {
+        return jobExperienceTypeRepository.findAll()
+                .stream()
+                .map(JobExperienceType::getName)
+                .toList();
+    }
+
+}
