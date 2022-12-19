c3966d52b9664a8aef7b86b4b7456c2d39ea7613
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-19 10:10:02 -0300

    YP-25: Create endpoint for get all levels of Language (#33)
    * YP-25: Method getAllLevels added in ILanguageService and LanguageLevelServiceImpl
    
    * YP-25: Endpoint getAllLevels added in LanguageController

---
+++ backend/src/main/java/com/portfolio/service/impl/LanguageLevelServiceImpl.java
@@ -0,0 +1,29 @@
+package com.portfolio.service.impl;
+
+import com.portfolio.model.entity.LanguageLevel;
+import com.portfolio.repository.ILanguageLevelRepository;
+import com.portfolio.service.ILanguageLevelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LanguageLevelServiceImpl implements ILanguageLevelService {
+
+    private final ILanguageLevelRepository languageLevelRepository;
+
+    @Autowired
+    public LanguageLevelServiceImpl(ILanguageLevelRepository languageLevelRepository){
+        this.languageLevelRepository = languageLevelRepository;
+    }
+
+    @Override
+    public List<String> getAllLevels() {
+        return languageLevelRepository.findAll()
+                .stream()
+                .map(LanguageLevel::getName)
+                .toList();
+    }
+
+}
