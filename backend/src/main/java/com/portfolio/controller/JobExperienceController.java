cd1249ba273c1af676e14171aceccdfd6bf96659
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-20 02:00:42 -0300

    YP-31: Endpoint delete added in JobExperienceCotroller

--- backend/src/main/java/com/portfolio/controller/JobExperienceController.java
+++ backend/src/main/java/com/portfolio/controller/JobExperienceController.java
@@ -6,9 +6,8 @@ import com.portfolio.service.IJobExperienceTypeService;
 import com.portfolio.service.ISeniorityTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,6 +41,13 @@ public class JobExperienceController {
     @GetMapping("/senioritys/types")
     public ResponseEntity<List<String>> getAllSeniorityTypes(){
         return ResponseEntity.ok().body(seniorityTypeService.getAllSeniorityTypes());
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
+        jobExperienceService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
