342c38c0a098a5276be540cdf146f4f983fec027
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-23 00:51:17 -0300

    fix/YP-01: Data and URLs modified in data.sql (#45)

--- database/data.sql
+++ database/data.sql
@@ -8,7 +8,8 @@ INSERT INTO roles (ROLE_NAME)
 -- INSERTS OF USERS
 
 INSERT INTO users (USER_NAME, EMAIL, USER_PASSWORD, CREATION_DATE, ROLE_ID) 
-	VALUES ('leandro_deferrari', 'leandro_deferrari@hotmail.com', '$2a$10$tcYZ7Vi5yzKlQhIc8kogvu8ETQ9ihqobpCzkiwEvSGlHCvXPV0RAC', CURRENT_TIMESTAMP(), 1);
+	VALUES ('leandro_deferrari', 'leandro_deferrari@hotmail.com', 
+			'$2a$10$tcYZ7Vi5yzKlQhIc8kogvu8ETQ9ihqobpCzkiwEvSGlHCvXPV0RAC', CURRENT_TIMESTAMP(), 1);
 
 -- INSERTS OF CITIES
 
@@ -64,34 +65,51 @@ INSERT INTO addresses (CITY_ID, LOCALITY_ID, COUNTRY_ID)
     
 INSERT INTO addresses (CITY_ID, LOCALITY_ID, PROVINCE_ID, COUNTRY_ID) 
 	VALUES (3, 3, 1, 1);
-    
+
 INSERT INTO addresses (CITY_ID, LOCALITY_ID, PROVINCE_ID, COUNTRY_ID)  
 	VALUES (4, 4, 2, 1);
-    
+
 -- INSERTS OF PERSONS
 
 INSERT INTO persons (FIRST_NAME, LAST_NAME, PROFILE_PHOTO_URL, TITLE, ABOUT_ME, EMAIL, BANNER_URL, LINKEDIN_URL, GITHUB_URL, CV_URL, ADDRESS_ID) 
-	VALUES ('Leandro', 'Deferrari Arevalo', 'www.prueba.com/foto-perfil', 
-			'Java Developer', 'bla bla bla bla bla bla', 'leandro_deferrari@hotmail.com', 
-            'www.prueba.com/banner', 'https://www.linkedin.com/in/javadeveloper-leandrodeferrariarevalo', 
-            'https://github.com/leandrodeferrari', 'www.prueba.com/cv', 1);
-    
+	VALUES ('Leandro', 'Deferrari Arevalo', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671724862/portfolio/foto-de-perfil_sx6idg.jpg', 
+			'Java Developer', 'Queriendo conseguir mi primer empleo IT, en donde pueda ampliar mis conocimientos, formar parte de un equipo de trabajo y aportar valor a la empresa.', 
+            'leandro_deferrari@hotmail.com', 
+            'https://res.cloudinary.com/dwirzebm8/image/upload/v1671724894/portfolio/banner_oinhiu.jpg', 
+            'https://www.linkedin.com/in/leandrodeferrari/', 
+            'https://github.com/leandrodeferrari', 'https://drive.google.com/file/d/1fao9swtIL21JLkNs94Qc4xZ2ePZQA8my/view?usp=sharing', 1);
+
 -- INSERTS OF TECHNOLOGIES
 
 INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
-	VALUES ('Spring boot', 'www.prueba.com/spring_boot', 1);
+	VALUES ('Spring', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671738733/portfolio/springio-ar21_mcexct.svg', 1);
     
 INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
-	VALUES ('Angular', 'www.prueba.com/angular', 1);
+	VALUES ('Angular', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671738847/portfolio/angular-ar21_jwtfeo.svg', 1);
     
 INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
-	VALUES ('MySQL', 'www.prueba.com/mysql', 1);
+	VALUES ('MySQL', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671738921/portfolio/mysql-ar21_l7oj2z.svg', 1);
     
 INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
-	VALUES ('SQL Server', 'www.prueba.com/sql-server', 1);
+	VALUES ('Jira', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671739425/portfolio/atlassian_jira-ar21_focdng.svg', 1);
     
 INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
-	VALUES ('Java', 'www.prueba.com/java', 1);
+	VALUES ('Java', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671739256/portfolio/java-ar21_id7yeo.svg', 1);
+    
+INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
+	VALUES ('GitHub', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671739340/portfolio/github-ar21_bulfdj.svg', 1);
+    
+INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
+	VALUES ('TypeScript', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671739563/portfolio/typescriptlang-ar21_sdnsed.svg', 1);
+    
+INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
+	VALUES ('Bootstrap', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671739696/portfolio/getbootstrap-ar21_nfo6xh.svg', 1);
+    
+INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
+	VALUES ('HTML', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671739801/portfolio/w3_html5-ar21_vjc09g.svg', 1);
+    
+INSERT INTO technologies (TECHNOLOGY_NAME, LOGO_URL, PERSON_ID) 
+	VALUES ('CSS', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671739875/portfolio/w3_css-ar21_xuetl8.svg', 1);
     
 -- INSERTS OF LANGUAGE_LEVELS
 
@@ -121,15 +139,17 @@ INSERT INTO languages (LANGUAGE_NAME, PERSON_ID, LANGUAGE_LEVEL_ID)
 -- INSERTS OF INSTITUTES_UNIVERSITIES
 
 INSERT INTO institutes_universities (INSTITUTE_UNIVERSITY_NAME, LOGO_URL, ADDRESS_ID) 
-	VALUES ('Instituto de Formación Tecnica Superior (IFTS) N° 11', 'www.prueba.com/logo', 3);
+	VALUES ('Instituto de Formación Tecnica Superior (IFTS) N° 11', 
+			'https://res.cloudinary.com/dwirzebm8/image/upload/v1671740375/portfolio/ifts-n-11-squarelogo-1664437707630_ttny6o.png', 3);
 
 INSERT INTO institutes_universities (INSTITUTE_UNIVERSITY_NAME, LOGO_URL, ADDRESS_ID) 
-	VALUES ('Egg Educación', 'www.prueba.com/logo', 4);
+	VALUES ('Egg Educación', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671740448/portfolio/egg-completo_rwjgfs.png', 4);
 
 -- INSERTS OF COURSES
 
 INSERT INTO courses (TITLE, IS_APPROVED, CERTIFICATE_URL, START_DATE, END_DATE, PERSON_ID, INSTITUTE_UNIVERSITY_ID) 
-	VALUES ('FullStack Developer', 1, 'www.prueba.com/certificado', '2021-11-03', '2022-09-29', 1, 2);
+	VALUES ('FullStack Developer', 1, 'https://drive.google.com/file/d/1jkKNvYbJd-Ag2m1hr89DyQNYWGa3BPzE/view?usp=sharing', 
+			'2021-11-03', '2022-09-29', 1, 2);
 
 -- INSERTS OF CAREER_TYPES
 
@@ -148,12 +168,12 @@ INSERT INTO careers (CAREER_NAME, START_DATE, END_DATE, IS_FINISHED, APPROVED_SU
 INSTITUTE_UNIVERSITY_ID, CAREER_TYPE_ID) 
 	VALUES ('Desarrollo de Software', '2022-08-16', '2024-12-01', 0, 4, 23, 
 			'https://www.ifts11.com/wp/carreras/plan-de-desarrollo-de-software/#page-content',
-            'bla bla bla bla bla', 1, 1, 1);
+            'Perfil capacitado para producir artefactos de software (reutilizando elementos existentes o programándolos enteramente), cumpliendo con los criterios de realización establecidos para las mismas, en el marco de un equipo de trabajo organizado por proyecto.', 1, 1, 1);
 
 -- INSERTS OF BUSINESSES
 
 INSERT INTO businesses (BUSINESS_NAME, LOGO_URL, ADDRESS_ID) 
-	VALUES ('Alkemy', 'www.prueba.com/alkemy', 2);
+	VALUES ('Alkemy', 'https://res.cloudinary.com/dwirzebm8/image/upload/v1671740828/portfolio/alkemy_uui7ip.jpg', 2);
     
 -- INSERTS OF PROJECT_TYPES
 
@@ -166,8 +186,9 @@ INSERT INTO project_types (PROJECT_TYPE_NAME)
 -- INSERTS OF PROJECTS
 
 INSERT INTO projects (PROJECT_NAME, START_DATE, END_DATE, DESCRIPT, IMAGE_URL, EVIDENCE_URL, PROJECT_TYPE_ID, PERSON_ID, BUSINESS_ID) 
-	VALUES ('AlkyBank', '2022-10-31', '2022-11-13', 'Skill-Up Java: Desarrollo, en equipo, de una Wallet Digital', 
-			'www.prueba.com/imagen', 'https://github.com/stanagatha/Skill-Up-Java-Wallet', 2, 1, 1);
+	VALUES ('AlkyBank', '2022-10-31', '2022-11-13', 'Realización del BackEnd de una Wallet Digital (Alkybank) conformado por un equipo de 8 integrantes (incluyéndome) durante 2 semanas. Utilizando las metodologías ágiles SCRUM y KANBAN (Jira). Diseñada con Java (Spring boot) y MySQL para la base de datos.', 
+			'https://res.cloudinary.com/dwirzebm8/image/upload/v1671740524/portfolio/imagen-alkybank_fcm2yx.png', 
+            'https://drive.google.com/file/d/1FVo2MzVsJJt-Gxq1zHSP0MJagQAE3_pI/view', 2, 1, 1);
 
 -- INSERTS OF SENIORITY_TYPES
 
@@ -197,4 +218,4 @@ INSERT INTO job_experience_types (JOB_EXPERIENCE_TYPE_NAME)
 -- INSERTS OF JOB_EXPERIENCES
 
 INSERT INTO job_experiences (JOB_POSITION_NAME, START_DATE, IS_CURRENT_JOB, DESCRIPT, SENIORITY_TYPE_ID, PERSON_ID, JOB_EXPERIENCE_TYPE_ID, BUSINESS_ID) 
-	VALUES ('FullStack Java Developer', '2022-12-01', 1, 'Tareas varias con Angular y Java', 2, 1, 2, 1);
+	VALUES ('Trabajo ficticio', '2022-12-01', 1, 'Tareas varias con Angular y Java', 2, 1, 2, 1);
