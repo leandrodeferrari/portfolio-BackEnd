d20ac6684331d7077d6701a18924597ceb3b33c7
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-24 19:42:35 -0300

    YP-41: Creation DTO InstituteUniversityInDto

---
+++ backend/src/main/java/com/portfolio/dto/request/InstituteUniversityInDto.java
@@ -0,0 +1,24 @@
+package com.portfolio.dto.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+public class InstituteUniversityInDto implements Serializable {
+
+    @NotNull(message = "The name cannot be null")
+    @NotBlank(message = "The name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "The logo URL cannot be blank")
+    private String logoUrl;
+
+    @NotNull(message = "The address ID cannot be null")
+    @Min(value = 1, message = "The address ID cannot be less than 1")
+    private Integer addressId;
+
+}
