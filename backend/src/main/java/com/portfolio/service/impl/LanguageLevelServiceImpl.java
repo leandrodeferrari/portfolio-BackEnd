d9d56501d451ca9881a523f57ebaba999926d2ab
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-24 02:05:42 -0300

    YP-38: Method findById added in ILanguageLevelService and LanguageLevelServiceImpl

--- backend/src/main/java/com/portfolio/service/impl/LanguageLevelServiceImpl.java
+++ backend/src/main/java/com/portfolio/service/impl/LanguageLevelServiceImpl.java
@@ -3,6 +3,7 @@ package com.portfolio.service.impl;
 import com.portfolio.model.entity.LanguageLevel;
 import com.portfolio.repository.ILanguageLevelRepository;
 import com.portfolio.service.ILanguageLevelService;
+import com.portfolio.util.ValidationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,15 @@ public class LanguageLevelServiceImpl implements ILanguageLevelService {
                 .stream()
                 .map(LanguageLevel::getName)
                 .toList();
+    }
+
+    @Override
+    public LanguageLevel findById(Integer id) {
+
+        ValidationUtil.validateId(id);
+
+        return languageLevelRepository.findById(id).orElseThrow();
+
     }
 
 }
