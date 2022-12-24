7892b0d3143ff21b1a148c64522b0cea39c20348
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-24 18:34:43 -0300

    YP-39: Method findById added in IProjectTypeService and ProjectTypeServiceImpl

--- backend/src/main/java/com/portfolio/service/IProjectTypeService.java
+++ backend/src/main/java/com/portfolio/service/IProjectTypeService.java
@@ -1,9 +1,13 @@
 package com.portfolio.service;
 
+import com.portfolio.model.entity.ProjectType;
+
 import java.util.List;
 
 public interface IProjectTypeService {
 
     List<String> getAllTypes();
+
+    ProjectType findById(Integer id);
 
 }
