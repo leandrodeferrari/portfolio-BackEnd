14b1ea69f9ab2fa64af45c15471b830d21733f88
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-24 20:13:41 -0300

    YP-42: Create endpoint for create a Business (#52)
    * YP-42: Creation DTO BusinessInDto
    
    * YP-42: Method businessInDtoToBusiness added in IBusinessMapper
    
    * YP-42: Method create added in IBusinessService and BusinessServiceImpl
    
    * YP-42: Endpoint create added in BusinessController

--- backend/src/main/java/com/portfolio/mapper/IBusinessMapper.java
+++ backend/src/main/java/com/portfolio/mapper/IBusinessMapper.java
@@ -1,10 +1,12 @@
 package com.portfolio.mapper;
 
+import com.portfolio.dto.request.BusinessInDto;
 import com.portfolio.dto.response.BusinessDto;
 import com.portfolio.model.entity.Business;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
@@ -13,5 +15,11 @@ public interface IBusinessMapper {
 
     @Mapping(target = "addressDto", source = "address")
     BusinessDto businessToBusinessDto(Business business);
+
+    @Mappings({
+            @Mapping(target = "address", ignore = true),
+            @Mapping(target = "id", ignore = true)
+    })
+    Business businessInDtoToBusiness(BusinessInDto businessInDto);
 
 }
