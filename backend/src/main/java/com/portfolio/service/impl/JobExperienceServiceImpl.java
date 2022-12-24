ecfb1d38bc7d1811f66cced9e5fd40bf869f490a
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-24 01:23:33 -0300

    YP-37: Method create added in IJobExperienceService and JobExperienceServiceImpl

--- backend/src/main/java/com/portfolio/service/impl/JobExperienceServiceImpl.java
+++ backend/src/main/java/com/portfolio/service/impl/JobExperienceServiceImpl.java
@@ -1,9 +1,12 @@
 package com.portfolio.service.impl;
 
+import com.portfolio.dto.request.JobExperienceInDto;
 import com.portfolio.dto.response.JobExperienceDto;
 import com.portfolio.mapper.IJobExperienceMapper;
+import com.portfolio.model.entity.JobExperience;
 import com.portfolio.repository.IJobExperienceRepository;
-import com.portfolio.service.IJobExperienceService;
+import com.portfolio.service.*;
+import com.portfolio.util.PersonUtil;
 import com.portfolio.util.ValidationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,14 +18,26 @@ import java.util.stream.Collectors;
 @Service
 public class JobExperienceServiceImpl implements IJobExperienceService {
 
+    private final IBusinessService businessService;
     private final IJobExperienceMapper jobExperienceMapper;
     private final IJobExperienceRepository jobExperienceRepository;
+    private final IJobExperienceTypeService jobExperienceTypeService;
+    private final IPersonService personService;
+    private final ISeniorityTypeService seniorityTypeService;
 
     @Autowired
-    public JobExperienceServiceImpl(IJobExperienceMapper jobExperienceMapper,
-                                    IJobExperienceRepository jobExperienceRepository){
+    public JobExperienceServiceImpl(IBusinessService businessService,
+                                    IJobExperienceMapper jobExperienceMapper,
+                                    IJobExperienceRepository jobExperienceRepository,
+                                    IJobExperienceTypeService jobExperienceTypeService,
+                                    IPersonService personService,
+                                    ISeniorityTypeService seniorityTypeService){
+        this.businessService = businessService;
         this.jobExperienceMapper = jobExperienceMapper;
         this.jobExperienceRepository = jobExperienceRepository;
+        this.jobExperienceTypeService = jobExperienceTypeService;
+        this.personService = personService;
+        this.seniorityTypeService = seniorityTypeService;
     }
 
     @Override
@@ -41,6 +56,31 @@ public class JobExperienceServiceImpl implements IJobExperienceService {
         jobExperienceRepository.findById(id).orElseThrow();
 
         jobExperienceRepository.deleteById(id);
+
+    }
+
+    @Transactional
+    @Override
+    public JobExperienceDto create(JobExperienceInDto jobExperienceInDto) {
+
+        JobExperience jobExperience = jobExperienceMapper
+                .JobExperienceInDtoToJobExperience(jobExperienceInDto);
+
+        jobExperience.setPerson(personService.findByEmail(PersonUtil.EMAIL));
+
+        if(jobExperienceInDto.getBusinessId() != null){
+            jobExperience.setBusiness(businessService.findById(jobExperienceInDto.getBusinessId()));
+        }
+
+        jobExperience.setJobExperienceType(jobExperienceTypeService
+                .findById(jobExperienceInDto.getJobExperienceTypeId()));
+
+        jobExperience.setSeniorityType(seniorityTypeService
+                .findById(jobExperienceInDto.getSeniorityTypeId()));
+
+        jobExperienceRepository.save(jobExperience);
+
+        return jobExperienceMapper.jobExperienceToJobExperienceDto(jobExperience);
 
     }
 
