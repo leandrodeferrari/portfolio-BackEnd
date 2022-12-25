2c58a26ee9ed54b88bf1ab5761312d91236fd30f
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-25 01:03:53 -0300

    YP-43: Method findByName added in ICountryRepository

--- backend/src/main/java/com/portfolio/repository/ICountryRepository.java
+++ backend/src/main/java/com/portfolio/repository/ICountryRepository.java
@@ -4,9 +4,11 @@ import com.portfolio.model.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ICountryRepository extends JpaRepository<Country, Integer> {
 
-
+    Optional<Country> findByName(String name);
 
 }
