392571765c6327c1254b7ae2922319e43a82a7c7
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-25 01:04:15 -0300

    YP-43: Method findByName added in IProvinceRepository

--- backend/src/main/java/com/portfolio/repository/IProvinceRepository.java
+++ backend/src/main/java/com/portfolio/repository/IProvinceRepository.java
@@ -4,9 +4,11 @@ import com.portfolio.model.entity.Province;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IProvinceRepository extends JpaRepository<Province, Integer> {
 
-
+    Optional<Province> findByName(String name);
 
 }
