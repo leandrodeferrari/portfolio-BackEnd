d55adac9cb375fb5831bcfa28eb2c35c7fd8a2d5
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-25 01:07:00 -0300

    YP-43: Methods findByName and create added in ICountryService and CountryServiceImpl

--- backend/src/main/java/com/portfolio/service/ICountryService.java
+++ backend/src/main/java/com/portfolio/service/ICountryService.java
@@ -1,9 +1,15 @@
 package com.portfolio.service;
 
+import com.portfolio.model.entity.Country;
+
 import java.util.List;
 
 public interface ICountryService {
 
     List<String> getAll();
+
+    Country findByName(String name);
+
+    String create(String name);
 
 }
--- backend/src/main/java/com/portfolio/service/impl/CountryServiceImpl.java
+++ backend/src/main/java/com/portfolio/service/impl/CountryServiceImpl.java
@@ -3,8 +3,10 @@ package com.portfolio.service.impl;
 import com.portfolio.model.entity.Country;
 import com.portfolio.repository.ICountryRepository;
 import com.portfolio.service.ICountryService;
+import com.portfolio.util.ValidationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,6 +26,28 @@ public class CountryServiceImpl implements ICountryService {
                 .stream()
                 .map(Country::getName)
                 .toList();
+    }
+
+    @Override
+    public Country findByName(String name) {
+
+        ValidationUtil.validateName(name);
+
+        return countryRepository.findByName(name).orElseThrow();
+
+    }
+
+    @Transactional
+    @Override
+    public String create(String name) {
+
+        ValidationUtil.validateName(name);
+
+        Country country = new Country();
+        country.setName(name);
+
+        return countryRepository.save(country).getName();
+
     }
 
 }
