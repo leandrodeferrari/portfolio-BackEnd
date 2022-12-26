c6e7b051319c185e0d5c3e0200bf057c0fa44c0c
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-26 02:43:32 -0300

    YP-51: Open Api configuration added in package config

---
+++ backend/src/main/java/com/portfolio/config/OpenApiConfig.java
@@ -0,0 +1,47 @@
+package com.portfolio.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+@Configuration
+public class OpenApiConfig {
+
+    private final String name;
+    private final String apiVersion;
+
+    public OpenApiConfig (@Value("Portfolio") String name,
+                          @Value("0.0.1") String apiVersion) {
+        this.name = name;
+        this.apiVersion = apiVersion;
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "Bearer Authentication";
+        final String apiTitle = String.format("%s API", StringUtils.capitalize(name));
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                            .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("Bearer")
+                                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title(apiTitle)
+                        .version(apiVersion)
+                        .description("Portfolio of Leandro Deferrari \uD83E\uDD73<br><br>" +
+                                        "<a href=\"https://github.com/leandrodeferrari/portfolio-BackEnd\" target=_blank>GitHub repository</a>"));
+
+    }
+
+}
