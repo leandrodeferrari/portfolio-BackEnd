e2f6024cee5a3c574b6665754926f0ad50c6a33b
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-26 14:33:04 -0300

    YP-55: Career documentation added in CareerController

--- backend/src/main/java/com/portfolio/controller/CareerController.java
+++ backend/src/main/java/com/portfolio/controller/CareerController.java
@@ -4,6 +4,8 @@ import com.portfolio.dto.request.CareerInDto;
 import com.portfolio.dto.response.CareerDto;
 import com.portfolio.service.ICareerService;
 import com.portfolio.service.ICareerTypeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Career", description = "Career Controller")
 @RestController
 @RequestMapping("/careers")
 public class CareerController {
@@ -26,16 +29,19 @@ public class CareerController {
         this.careerTypeService = careerTypeService;
     }
 
+    @Operation(description = "Get All Careers. Any User. Parameters: none")
     @GetMapping
     public ResponseEntity<List<CareerDto>> getAll(){
         return ResponseEntity.ok().body(careerService.getAll());
     }
 
+    @Operation(description = "Get All Career Types. Any User. Parameters: none")
     @GetMapping("/types")
     public ResponseEntity<List<String>> getAllTypes(){
         return ResponseEntity.ok().body(careerTypeService.getAllTypes());
     }
 
+    @Operation(description = "Delete a Career. Role: Admin. Parameters: Path Variable id")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
@@ -43,11 +49,13 @@ public class CareerController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Create a Career. User Authenticated. Parameters: Schema CareerInDto.")
     @PostMapping
     public ResponseEntity<CareerDto> create(@Valid @RequestBody CareerInDto careerInDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(careerService.create(careerInDto));
     }
 
+    @Operation(description = "Update a Career. User Authenticated. Parameters: Schema CareerInDto, Path Variable id.")
     @PutMapping("/{id}")
     public ResponseEntity<CareerDto> update(@Valid @RequestBody CareerInDto careerInDto,
                                             @PathVariable("id") Integer id){
