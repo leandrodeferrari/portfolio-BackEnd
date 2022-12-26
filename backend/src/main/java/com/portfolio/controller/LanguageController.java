dacda12a749ec17cd0570499f7c4877de8c4bb70
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2022-12-26 15:59:52 -0300

    YP-59: Language documentation (#70)

--- backend/src/main/java/com/portfolio/controller/LanguageController.java
+++ backend/src/main/java/com/portfolio/controller/LanguageController.java
@@ -4,6 +4,8 @@ import com.portfolio.dto.request.LanguageInDto;
 import com.portfolio.dto.response.LanguageDto;
 import com.portfolio.service.ILanguageLevelService;
 import com.portfolio.service.ILanguageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Language", description = "Language Controller")
 @RestController
 @RequestMapping("/languages")
 public class LanguageController {
@@ -27,16 +30,19 @@ public class LanguageController {
         this.languageService = languageService;
     }
 
+    @Operation(description = "Get All Languages. Any User. Parameters: none")
     @GetMapping
     public ResponseEntity<List<LanguageDto>> getAll(){
         return ResponseEntity.ok().body(languageService.getAll());
     }
 
+    @Operation(description = "Get All Language Levels. Any User. Parameters: none")
     @GetMapping("/levels")
     public ResponseEntity<List<String>> getAllLevels(){
         return ResponseEntity.ok().body(languageLevelService.getAllLevels());
     }
 
+    @Operation(description = "Delete a Language. Role: Admin. Parameters: Path Variable id")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
@@ -44,11 +50,13 @@ public class LanguageController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Create a Language. User Authenticated. Parameters: Schema LanguageInDto.")
     @PostMapping
     public ResponseEntity<LanguageDto> create(@Valid @RequestBody LanguageInDto languageInDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(languageService.create(languageInDto));
     }
 
+    @Operation(description = "Update a Language. User Authenticated. Parameters: Schema LanguageInDto, Path Variable id.")
     @PutMapping("/{id}")
     public ResponseEntity<LanguageDto> update(@Valid @RequestBody LanguageInDto languageInDto,
                                               @PathVariable("id") Integer id){
