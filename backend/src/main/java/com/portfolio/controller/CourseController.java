7745b14c6f6a4b568e9929f39c2dea3534983584
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-26 15:37:52 -0300

    YP-56: Course documentation in CourseController

--- backend/src/main/java/com/portfolio/controller/CourseController.java
+++ backend/src/main/java/com/portfolio/controller/CourseController.java
@@ -3,6 +3,8 @@ package com.portfolio.controller;
 import com.portfolio.dto.request.CourseInDto;
 import com.portfolio.dto.response.CourseDto;
 import com.portfolio.service.ICourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Course", description = "Course Controller")
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
@@ -23,11 +26,13 @@ public class CourseController {
         this.courseService = courseService;
     }
 
+    @Operation(description = "Get All Courses. Any User. Parameters: none")
     @GetMapping
     public ResponseEntity<List<CourseDto>> getAll(){
         return ResponseEntity.ok().body(courseService.getAll());
     }
 
+    @Operation(description = "Delete a Course. Role: Admin. Parameters: Path Variable id")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
@@ -35,11 +40,13 @@ public class CourseController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Create a Course. User Authenticated. Parameters: Schema CourseInDto.")
     @PostMapping
     public ResponseEntity<CourseDto> create(@Valid @RequestBody CourseInDto courseInDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(courseService.create(courseInDto));
     }
 
+    @Operation(description = "Update a Course. User Authenticated. Parameters: Schema CourseInDto, Path Variable id.")
     @PutMapping("/{id}")
     public ResponseEntity<CourseDto> update(@Valid @RequestBody CourseInDto courseInDto,
                                             @PathVariable("id") Integer id){
