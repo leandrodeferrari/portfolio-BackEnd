4c1fda8e30a05143380cc47c9742b399e8abfc87
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-26 15:50:58 -0300

    YP-58: JobExperience documentation in JobExperienceController

--- backend/src/main/java/com/portfolio/controller/JobExperienceController.java
+++ backend/src/main/java/com/portfolio/controller/JobExperienceController.java
@@ -5,6 +5,8 @@ import com.portfolio.dto.response.JobExperienceDto;
 import com.portfolio.service.IJobExperienceService;
 import com.portfolio.service.IJobExperienceTypeService;
 import com.portfolio.service.ISeniorityTypeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Job Experience", description = "JobExperience Controller")
 @RestController
 @RequestMapping("job-experiences")
 public class JobExperienceController {
@@ -31,21 +34,25 @@ public class JobExperienceController {
         this.seniorityTypeService = seniorityTypeService;
     }
 
+    @Operation(description = "Get All Job Experiences. Any User. Parameters: none")
     @GetMapping
     public ResponseEntity<List<JobExperienceDto>> getAll(){
         return ResponseEntity.ok().body(jobExperienceService.getAll());
     }
 
+    @Operation(description = "Get All Job Experience Types. Any User. Parameters: none")
     @GetMapping("/types")
     public ResponseEntity<List<String>> getAllTypes(){
         return ResponseEntity.ok().body(jobExperienceTypeService.getAllTypes());
     }
 
+    @Operation(description = "Get All Seniority Types. Any User. Parameters: none")
     @GetMapping("/senioritys/types")
     public ResponseEntity<List<String>> getAllSeniorityTypes(){
         return ResponseEntity.ok().body(seniorityTypeService.getAllSeniorityTypes());
     }
 
+    @Operation(description = "Delete a Job Experience. Role: Admin. Parameters: Path Variable id")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
@@ -53,11 +60,13 @@ public class JobExperienceController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Create a Job Experience. User Authenticated. Parameters: Schema JobExperienceInDto.")
     @PostMapping
     public ResponseEntity<JobExperienceDto> create(@Valid @RequestBody JobExperienceInDto jobExperienceInDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(jobExperienceService.create(jobExperienceInDto));
     }
 
+    @Operation(description = "Update a Job Experience. User Authenticated. Parameters: Schema JobExperienceInDto, Path Variable id.")
     @PutMapping("/{id}")
     public ResponseEntity<JobExperienceDto> update(@Valid @RequestBody JobExperienceInDto jobExperienceInDto,
                                                    @PathVariable("id") Integer id){
