f2fa4047ba218cd2f0e5281599016f53df5129cc
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-26 16:03:54 -0300

    YP-60: Person documentation in PersonController

--- backend/src/main/java/com/portfolio/controller/PersonController.java
+++ backend/src/main/java/com/portfolio/controller/PersonController.java
@@ -2,12 +2,15 @@ package com.portfolio.controller;
 
 import com.portfolio.dto.response.PersonDto;
 import com.portfolio.service.IPersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Person", description = "Person Controller")
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
@@ -19,6 +22,7 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @Operation(description = "Get a Person. Any User. Parameters: none")
     @GetMapping("/person")
     public ResponseEntity<PersonDto> getPerson(){
         return ResponseEntity.ok().body(personService.getPerson());
