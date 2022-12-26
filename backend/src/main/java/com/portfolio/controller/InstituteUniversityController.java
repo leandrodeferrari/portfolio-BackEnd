6d78ef99d1ce006fa0604e59a8353271ff947158
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-26 15:44:13 -0300

    YP-57: InstituteUniversity documentation in InstituteUniversityController

--- backend/src/main/java/com/portfolio/controller/InstituteUniversityController.java
+++ backend/src/main/java/com/portfolio/controller/InstituteUniversityController.java
@@ -3,6 +3,8 @@ package com.portfolio.controller;
 import com.portfolio.dto.request.InstituteUniversityInDto;
 import com.portfolio.dto.response.InstituteUniversityDto;
 import com.portfolio.service.IInstituteUniversityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Institute/University", description = "InstituteUniversity Controller")
 @RestController
 @RequestMapping("/institutes-universities")
 public class InstituteUniversityController {
@@ -22,11 +25,13 @@ public class InstituteUniversityController {
         this.instituteUniversityService = instituteUniversityService;
     }
 
+    @Operation(description = "Get All Institutes/Universities. Any User. Parameters: none")
     @GetMapping
     public ResponseEntity<List<InstituteUniversityDto>> getAll(){
         return ResponseEntity.ok().body(instituteUniversityService.getAll());
     }
 
+    @Operation(description = "Create a Institute/University. User Authenticated. Parameters: Schema InstituteUniversityInDto.")
     @PostMapping
     public ResponseEntity<InstituteUniversityDto> create(@Valid @RequestBody InstituteUniversityInDto instituteUniversityInDto){
         return ResponseEntity.status(HttpStatus.CREATED)
