e1a0b39bd19effe9110321ee7eb3aecab84ef37a
Leandro Deferrari <leandro_deferrari@hotmail.com>
2022-12-26 14:01:01 -0300

    YP-54: Business documentation added in BusinessCotroller

--- backend/src/main/java/com/portfolio/controller/BusinessController.java
+++ backend/src/main/java/com/portfolio/controller/BusinessController.java
@@ -3,6 +3,8 @@ package com.portfolio.controller;
 import com.portfolio.dto.request.BusinessInDto;
 import com.portfolio.dto.response.BusinessDto;
 import com.portfolio.service.IBusinessService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Business", description = "Business Controller")
 @RestController
 @RequestMapping("/businesses")
 public class BusinessController {
@@ -22,11 +25,13 @@ public class BusinessController {
         this.businessService = businessService;
     }
 
+    @Operation(description = "Get All Business. Any User. Parameters: none")
     @GetMapping
     public ResponseEntity<List<BusinessDto>> getAll(){
         return ResponseEntity.ok().body(businessService.getAll());
     }
 
+    @Operation(description = "Create a Business. User Authenticated. Parameters: Schema BusinessInDto.")
     @PostMapping
     public ResponseEntity<BusinessDto> create(@Valid @RequestBody BusinessInDto businessInDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(businessService.create(businessInDto));
