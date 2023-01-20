f4065874c0a260020eda13ee26014e5900f90821
Leandro Deferrari <leandro_deferrari@hotmail.com>
2023-01-20 12:38:45 -0300

    YP-68: Package target deleted of gitignore

---
+++ backend/target/generated-sources/annotations/com/portfolio/mapper/IAddressMapperImpl.java
@@ -0,0 +1,96 @@
+package com.portfolio.mapper;
+
+import com.portfolio.dto.response.AddressDto;
+import com.portfolio.model.entity.Address;
+import com.portfolio.model.entity.City;
+import com.portfolio.model.entity.Country;
+import com.portfolio.model.entity.Locality;
+import com.portfolio.model.entity.Province;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-01-20T12:29:36-0300",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.5 (Oracle Corporation)"
+)
+@Component
+public class IAddressMapperImpl implements IAddressMapper {
+
+    @Override
+    public AddressDto addressToAddressDto(Address address) {
+        if ( address == null ) {
+            return null;
+        }
+
+        AddressDto addressDto = new AddressDto();
+
+        addressDto.setCityName( addressCityName( address ) );
+        addressDto.setLocalityName( addressLocalityName( address ) );
+        addressDto.setProvinceName( addressProvinceName( address ) );
+        addressDto.setCountryName( addressCountryName( address ) );
+        addressDto.setId( address.getId() );
+
+        return addressDto;
+    }
+
+    private String addressCityName(Address address) {
+        if ( address == null ) {
+            return null;
+        }
+        City city = address.getCity();
+        if ( city == null ) {
+            return null;
+        }
+        String name = city.getName();
+        if ( name == null ) {
+            return null;
+        }
+        return name;
+    }
+
+    private String addressLocalityName(Address address) {
+        if ( address == null ) {
+            return null;
+        }
+        Locality locality = address.getLocality();
+        if ( locality == null ) {
+            return null;
+        }
+        String name = locality.getName();
+        if ( name == null ) {
+            return null;
+        }
+        return name;
+    }
+
+    private String addressProvinceName(Address address) {
+        if ( address == null ) {
+            return null;
+        }
+        Province province = address.getProvince();
+        if ( province == null ) {
+            return null;
+        }
+        String name = province.getName();
+        if ( name == null ) {
+            return null;
+        }
+        return name;
+    }
+
+    private String addressCountryName(Address address) {
+        if ( address == null ) {
+            return null;
+        }
+        Country country = address.getCountry();
+        if ( country == null ) {
+            return null;
+        }
+        String name = country.getName();
+        if ( name == null ) {
+            return null;
+        }
+        return name;
+    }
+}
