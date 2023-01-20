49725c3bd0485d53538b6ef9e8f199a7a90465e9
Leandro Deferrari <leandro_deferrari@hotmail.com>
2023-01-20 11:44:00 -0300

    YP-67: Endpoint sendEmail added in PersonController

--- backend/src/main/java/com/portfolio/controller/PersonController.java
+++ backend/src/main/java/com/portfolio/controller/PersonController.java
@@ -1,9 +1,12 @@
 package com.portfolio.controller;
 
+import com.portfolio.dto.request.EmailDto;
 import com.portfolio.dto.response.AboutDto;
 import com.portfolio.dto.response.BannerDto;
 import com.portfolio.dto.response.ContactDto;
 import com.portfolio.dto.response.PersonDto;
+import com.portfolio.exception.EmailException;
+import com.portfolio.service.IEmailService;
 import com.portfolio.service.IPersonService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,10 +14,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200", "www.leandrodeferrari.com.ar"})
 @Tag(name = "Person", description = "Person Controller")
@@ -22,10 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/persons")
 public class PersonController {
 
+    private final IEmailService emailService;
     private final IPersonService personService;
 
     @Autowired
-    public PersonController(IPersonService personService){
+    public PersonController(IEmailService emailService, IPersonService personService){
+        this.emailService = emailService;
         this.personService = personService;
     }
 
@@ -51,6 +55,11 @@ public class PersonController {
     @GetMapping("/about")
     public ResponseEntity<AboutDto> getAbout(){
         return ResponseEntity.ok().body(personService.getAbout());
+    }
+
+    @PostMapping("/contact")
+    public ResponseEntity<Map<String, String>> sendEmail(@RequestBody EmailDto emailDto) throws EmailException {
+        return ResponseEntity.ok().body(emailService.sendEMail(emailDto));
     }
 
 }
